f3ef6c7ac169d1c8ffdd5b642c9a69b6caf9c64e
Govinda Upadhya <upadhyagovinda01@gmail.com>
2023-10-21 22:23:33 +0530

    Configured the admin panel and added 3 products

--- shop/models.py
+++ shop/models.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     category=models.CharField(max_length=100)
     description=models.TextField()
     image=models.ImageField(upload_to="images")
+
+    def __str__(self):
+        return f"{self.title} {self.price} {self.category} "
