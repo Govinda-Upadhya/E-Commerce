5d28f4bcffb5ade5c6f268c05f801a056394522d
Govinda Upadhya <upadhyagovinda01@gmail.com>
2023-10-21 21:02:30 +0530

    created a model for the products and made migrations for the creation of database

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title=models.CharField(max_length=200)
+    price=models.FloatField()
+    discounted_price=models.FloatField()
+    category=models.CharField(max_length=100)
+    description=models.TextField()
+    image=models.ImageField(upload_to="images")
