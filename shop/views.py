84c28be08a6277d649b78ce06a362ccc74302b3b
Govinda Upadhya <upadhyagovinda01@gmail.com>
2023-10-22 13:41:23 +0530

    done

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import SearchForm
+from django.core.paginator import Paginator,EmptyPage
 # Create your views here.
 
 def index(request):
+   
     products=Product.objects.all()
+    
+    search_form=request.GET.get("item name")
+    if search_form != None :
+        products=Product.objects.filter(title__icontains=search_form)
+    p=Paginator(products,4)   
     return render(request,"shop/index.html",{
-        "items":products
+        "items":products,
+        
     })
 
